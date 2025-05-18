a4e207b0e636fe6d2375008cfe31d1bde6f25a75
sanjildev <shakyasanjil00@gmail.com>
2025-05-18 09:10:38 +0545

    first commit

---
+++ server.ts
@@ -0,0 +1,11 @@
+import app from "./src/app";
+import { envConfig } from "./src/config/config";
+
+function startServer(){
+const port=envConfig.port || 4000
+app.listen(port,()=>{
+    console.log(`Server has been started at port ${port}`);
+    
+})
+}
+startServer()
---
+++ src/config/config.ts
@@ -0,0 +1,5 @@
+import { config } from "dotenv"
+config()
+export const envConfig={
+    port:process.env.PORT
+}
